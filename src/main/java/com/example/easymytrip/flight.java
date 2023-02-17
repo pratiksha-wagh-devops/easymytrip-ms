7bd13bee81b089e12ec69936f1eed58c64335713
pratiksha-wagh-devops <pratikshawagh129@gmail.com>
2023-02-17 20:30:28 +0530

    coderefactor

---
+++ src/main/java/com/example/easymytrip/flight.java
@@ -0,0 +1,10 @@
+package com.example.easymytrip;
+
+import org.springframework.boot.SpringApplication;
+
+public class flight {
+    public String getData() {
+        return "please book your xyz flight and choose your fav airlines indigo.";
+    }
+
+}
